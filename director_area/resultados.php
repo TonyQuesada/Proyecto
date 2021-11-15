c0c21e41db4472c4cba55f395bf8261ab3d93345
TonyQuesada <Tonyquesadaxd@gmail.com>
2021-11-15 04:24:51 -0600

    Update resultados.php
    Avance Noche 14/11/21

---
+++ director_area/resultados.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+if(!isset($_SESSION['u_ID'])){
+    header('Location: index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../favicon.ico">
+        <link rel="stylesheet" href="../css/styles_general.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>        
+    </head>
+    <body>
+
+        <div class="cabecera">
+            <div class="perfil">
+                <div>
+                    <a class="a" href="cuenta.php">👤 <?php echo $_SESSION['u_Nombre']; ?></a>
+                    </br>
+                </div>                
+                <div>
+                    <a class="a" href="../util/logout.php">🚪 Salir</a>
+                </div>
+            </div>
+            <img class="logo" src="../favicon.png" width="67" height="62"> 
+            <a class="a" href="../index.php"><h2 class="h2">Sistema de Gestión de Control Interno</h2></a>
+        </div>
+
+        <div class="container">            
+            <div class="columna_der">
+                <a class="a" href="../administrador.php">Inicio</a>
+
+                <!-- Director de Área -->
+                <?php if ($_SESSION['u_idRol'] == 1){ ?>
+                    </br><a class="a" href="./metas.php"> Metas</a>
+                    </br><a class="a"href="./resultados.php" style="color: #031075; font-size: 21px; font-weight: bold;"> Resultados</a>
+                <?php } ?>
+                
+                <!-- Fiscalizador -->
+                <?php if ($_SESSION['u_idRol'] == 2){ ?>
+                    </br><a class="a" href="./asignar_roles.php"> Asignar Roles</a>
+                    </br><a class="a" href="./definir_alcance_metas.php"> Definir Alcance Metas</a>
+                    </br><a class="a" href="./departamentos.php"> Departamentos</a>
+                    </br><a class="a" href="./direccciones.php"> Direccciones</a>
+                    </br><a class="a" href="./establecer_fechas_evaluacion.php"> Establecer Fechas de Evaluación</a>
+                    </br><a class="a" href="./resultados.php"> Resultados</a>
+                <?php } ?>                        
+                
+                <!-- Encargado de Departamento -->
+                <?php if ($_SESSION['u_idRol'] == 3){ ?>
+                <?php } ?>                        
+                
+                <!-- Empleado -->
+                <?php if ($_SESSION['u_idRol'] == 4){ ?>
+                <?php } ?>                
+            </div>
+
+            <div class="panel">
+                <h2>Panel central</h2>
+            </div>
+
+        </div>
+        <script src="./common.js"></script>
+    </body>
+
+</html>
